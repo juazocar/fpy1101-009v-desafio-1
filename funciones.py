44f35c8d09b4f1e99846577148163c5fcc5f3ccc
Alumno Plaza Vespucio <pv-alumno@duoc.acad>
2026-05-28 19:37:14 -0400

    Corrección de errores CRUD estudiantes

--- funciones.py
+++ funciones.py
@@ -1,14 +1,6 @@
-"""
-Actividad DUOC UC - Debugging con Python
-Archivo de funciones: funciones.py
-
-Este archivo contiene las funciones del CRUD.
-Tiene errores intencionales de sintaxis, lógica y uso de parámetros.
-"""
-
 def agregar_estudiante(estudiantes):
     print("\n--- Agregar estudiante ---")
-    rut = input("Ingrese RUT: ")
+    rut = input("Ingrese rut con puntos y guion: ")
     nombre = input("Ingrese nombre: ")
     carrera = input("Ingrese carrera: ")
     edad = int(input("Ingrese edad: "))
@@ -20,7 +12,7 @@ def agregar_estudiante(estudiantes):
         "edad": edad
     }
 
-    estudiantes = estudiante
+    estudiantes.append (estudiante)
     print("Estudiante agregado correctamente")
 
 
@@ -34,7 +26,7 @@ def listar_estudiantes(estudiantes):
             print(f"RUT: {estudiantes[i]['rut']}")
             print(f"Nombre: {estudiantes[i]['nombre']}")
             print(f"Carrera: {estudiantes[i]['carrera']}")
-            print(f"Edad: {estudiantes['edad']}")
+            print(f"Edad: {estudiantes[i]['edad']}")
             print("------------------------")
 
 
@@ -44,7 +36,7 @@ def buscar_estudiante(estudiantes, rut):
     encontrado = False
 
     for estudiante in estudiantes:
-        if estudiante["nombre"] == rut:
+        if estudiante["rut"] == rut:
             print("Estudiante encontrado")
             print(f"RUT: {estudiante['rut']}")
             print(f"Nombre: {estudiante['nombre']}")
@@ -52,7 +44,7 @@ def buscar_estudiante(estudiantes, rut):
             print(f"Edad: {estudiante['edad']}")
             encontrado = True
 
-    if encontrado = False:
+    if encontrado == False:
         print("No se encontró el estudiante")
 
 
@@ -63,7 +55,7 @@ def actualizar_estudiante(estudiantes, rut):
         if estudiante["rut"] == rut:
             nuevo_nombre = input("Ingrese nuevo nombre: ")
             nueva_carrera = input("Ingrese nueva carrera: ")
-            nueva_edad = input("Ingrese nueva edad: ")
+            nueva_edad = int(input("Ingrese nueva edad: "))
 
             estudiante["nombre"] = nuevo_nombre
             estudiante["carrera"] = nueva_carrera
@@ -79,7 +71,7 @@ def eliminar_estudiante(estudiantes, rut):
     print("\n--- Eliminar estudiante ---")
 
     for estudiante in estudiantes:
-        if estudiante["rut"] = rut:
+        if estudiante["rut"] == rut:
             estudiantes.remove(estudiante)
             print("Estudiante eliminado correctamente")
             return
